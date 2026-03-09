df1c19c002e24509b8c30492b1925e16285883ed
Μάνος Σφυράκης <sfyr131@gmail.com>
2026-03-09 20:55:01 +0200

    Add Wikipedia ingestion service for sports data retrieval

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI(title="Sports Wiki RAG API")
+# app = FastAPI(title="Sports Wiki RAG API")
 
-@app.get("/")
-def root():
-    return {"message": "Sports RAG API is running"}
+# @app.get("/")
+# def root():
+#     return {"message": "Sports RAG API is running"}
+
+from services.wiki_service import search_wikipedia, get_page_content
+
+
+results = search_wikipedia("Lionel Messi")
+
+print(results)
+
+page = get_page_content(results[0])
+
+print(page["title"])
+print(page["content"][:500])
---
+++ app/services/wiki_service.py
@@ -0,0 +1,46 @@
+import wikipedia
+
+def search_wikipedia(query: str):
+    """Search Wikipedia for a given query and return top results."""
+    results = wikipedia.search(query)
+    return results
+
+# def get_page_content(title: str):
+#     """
+#     Fetch full contetnt of a Wikipedia page.
+#     """
+#     page = wikipedia.page(title)
+#     return {
+#         "title": page.title,
+#         "content": page.content,
+#         "url": page.url
+#     }
+
+def get_page_content(title: str):
+    """
+    Fetch full content of a Wikipedia page safely.
+    Tries to handle PageError and DisambiguationError.
+    """
+    try:
+        page = wikipedia.page(title)
+        return {
+            "title": page.title,
+            "content": page.content,
+            "url": page.url
+        }
+    except wikipedia.DisambiguationError as e:
+        # If disambiguation, pick the first option
+        first_option = e.options[0]
+        page = wikipedia.page(first_option)
+        return {
+            "title": page.title,
+            "content": page.content,
+            "url": page.url
+        }
+    except wikipedia.PageError:
+        # Page not found, return empty
+        return {
+            "title": title,
+            "content": "",
+            "url": ""
+        }
